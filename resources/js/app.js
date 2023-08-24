89723421ae3f9e36aff683b930c60d4bd0968d32
Guilherme Torres <torres.gnt94@gmail.com>
2023-08-24 00:06:14 -0300

    Configurações necessárias para componentes VUE

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp } from "vue";
+// import example from "@cmp/example.vue";
+
+const app = createApp();
+
+// app.component("example", example);
+
+app.mount("#app");
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -8,6 +8,7 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
@@ -16,8 +17,10 @@
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"
         integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 
-    <div class="container">
-        @yield('content')
+    <div id="app">
+        <div class="container">
+            @yield('content')
+        </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
